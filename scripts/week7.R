dffe04f051dcd086158b81ce6cb1f9a0ef0de478
sam <samwhite@uw.edu>
2018-05-08 13:44:43 -0700

    Add comments

--- scripts/week7.R
+++ scripts/week7.R
@@ -3,6 +3,8 @@
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
+
+# Create new table: fish by size
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
