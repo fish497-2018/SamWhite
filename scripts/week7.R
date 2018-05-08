77515a58a22e7ee6dd4b99c367a41c0177b0198b
sam <samwhite@uw.edu>
2018-05-08 13:43:37 -0700

    Change size limit to 200

--- scripts/week7.R
+++ scripts/week7.R
@@ -4,5 +4,5 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
 
